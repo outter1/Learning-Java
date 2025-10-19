b391dc06d71e08e87f1dc328cc302c8e3c888c5d
Gabriel Silva Bastos <160263825+outter1@users.noreply.github.com>
2025-10-19 14:26:45 -0300

    Add files via upload

---
+++ Var.java
@@ -0,0 +1,19 @@
+public class FirstProject {
+
+    public static void main(String[] args) {
+        // var -> (String) = text
+        //var -> (int) = number
+        //var -> (double) = fractional number
+        //var -> (boolean) = false or true
+
+    String login = "Hii";
+    int number1 = 10;
+    double percent = 24.33;
+    boolean isDriver = true;
+
+        System.out.println(login);
+        System.out.println(password);
+        System.out.println(percent);
+        System.out.println(isDriver);
+    }
+}
